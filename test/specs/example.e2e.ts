1528bbedd8f4c8bcbb7386f80a76022658432232
Nick Chursin <nicktestqa@yahoo.com>
2021-12-02 17:12:21 +0200

    init

---
+++ test/specs/example.e2e.ts
@@ -0,0 +1,19 @@
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+
+    xit('fails', async () => {
+        await expect("a").toBe("A");
+    });
+});
+
+
---
+++ wdio.conf.ts
@@ -0,0 +1,70 @@
+import * as fs from "fs";
+
+export const config: WebdriverIO.Config = {
+  hostname: "localhost",
+  port: 4444,
+  path: "/wd/hub",
+
+  specs: ["./test/specs/**/*.ts"],
+
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+
+  maxInstances: 10,
+
+  capabilities: [
+    {
+      maxInstances: 5,
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+    },
+  ],
+
+  // Level of logging verbosity: trace | debug | info | warn | error | silent
+  logLevel: "error",
+  //
+  // Set specific log levels per logger
+  // loggers:
+  // - webdriver, webdriverio
+  // - @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
+  // - @wdio/mocha-framework, @wdio/jasmine-framework
+  // - @wdio/local-runner
+  // - @wdio/sumologic-reporter
+  // - @wdio/cli, @wdio/config, @wdio/utils
+  // Level of logging verbosity: trace | debug | info | warn | error | silent
+  // logLevels: {
+  //     webdriver: 'info',
+  //     '@wdio/appium-service': 'info'
+  // },
+  //
+
+  bail: 0,
+  baseUrl: "https://the-internet.herokuapp.com/",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: "mocha",
+
+  reporters: [
+    "spec",
+    ["junit", { outputDir: "results/junit" }],
+    ["allure", { outputDir: "results/allure-results", disableWebdriverScreenshotsReporting: false }],
+  ],
+
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+
+  onPrepare() {
+    fs.rmdirSync("results", { recursive: true });
+  },
+
+
+  afterTest(test: any, context: any, result: any){
+    if (!result.passed) {
+      browser.takeScreenshot();
+    }
+  }
+};
